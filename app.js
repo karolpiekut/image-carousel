bb1bf9bafd740b218b34727dde649d49ba63b751
kpiekut <karol.piekut@pm.me>
2024-09-17 13:29:06 +0100

    add styles

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("test");
+const leftArrow = document.querySelector('#left-arrow');
+const rightArrow = document.querySelector('#right-arrow');
+const pic1 = document.querySelector('#id1');
+const pic2 = document.querySelector('#id2');
+let imageId = 0;
+
+function controls() {
+    if (this.id === "left-arrow"){
+        pic1.style.display = "none";
+        pic2.style.display = "block";
+    }
+}
+
+leftArrow.addEventListener('click', controls);
+rightArrow.addEventListener('click', controls);
+
+
+
+
+
+
+
