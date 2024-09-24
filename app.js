93083bc88118d1d35fe188d1410d73e739916ef1
kpiekut <karol.piekut@pm.me>
2024-09-24 13:20:27 +0100

    fixed controls

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 const leftArrow = document.querySelector('#left-arrow');
 const rightArrow = document.querySelector('#right-arrow');
-const pic1 = document.querySelector('#id1');
-const pic2 = document.querySelector('#id2');
-let imageId = 0;
+const galleryDivs = document.querySelectorAll('.image-div');
 
-function controls() {
-    if (this.id === "left-arrow"){
-        pic1.style.display = "none";
-        pic2.style.display = "block";
+let imageId = 1;
+let imageTopId = galleryDivs.length;
+
+function controls(par) {
+    if (imageId === imageTopId) {
+        if (par.id === "left-arrow") {
+            imageId--;
+            console.log(imageId);
+        } else if (par.id === "right-arrow") {
+            imageId = 1;
+            console.log(imageId);
+        }
+    } else if (imageId === 1) {
+        if (par.id === "left-arrow") {
+            imageId = imageTopId;
+            console.log(imageId);
+        } else if (par.id === "right-arrow") {
+            imageId++;
+            console.log(imageId);
+        }
+    } else {
+        if (par.id === "left-arrow") {
+            imageId--;
+            console.log(imageId);
+        } else if (par.id === "right-arrow") {
+            imageId++;
+            console.log(imageId);
+        }
     }
 }
 
-leftArrow.addEventListener('click', controls);
-rightArrow.addEventListener('click', controls);
+function makeItMove(){
+    for (let i = 0; i < galleryDivs.length; i++) {
+        if (Number(galleryDivs[i].id.slice(2)) !== imageId) {
+            document.querySelector(`#${galleryDivs[i].id}`).className = 'image-div hide';
+        } else {
+            document.querySelector(`#${galleryDivs[i].id}`).className = 'image-div show';
+        }
+    }
+}
 
+function together(){
+    controls(this);
+    makeItMove();
+}
 
-
-
-
+makeItMove();
+leftArrow.addEventListener('click', together);
+rightArrow.addEventListener('click', together);
 
 
